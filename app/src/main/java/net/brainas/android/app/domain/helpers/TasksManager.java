a021cb3ec864d52c3c16ed19dadf45878e75abfc
Kit Ushakov <kitushakoff@gmail.com>
2015-11-27 18:37:23 +0300

    Alpha 0.02

--- app/src/main/java/net/brainas/android/app/domain/helpers/TasksManager.java
+++ app/src/main/java/net/brainas/android/app/domain/helpers/TasksManager.java
@@ -6,26 +6,33 @@ import net.brainas.android.app.infrustructure.TaskDbHelper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Kit Ushakov on 11/9/2015.
  */
 public class TasksManager {
+    public enum GROUP_OF_TASKS {
+        ALL,
+        ACTIVE,
+        USED
+    }
+
     public TasksManager() {}
 
-    public List<Task> getTasks() {
+    public List<Task> getTasksFromDB(Map<String, Object> params) {
         BrainasApp app = (BrainasApp)BrainasApp.getAppContext();
         TaskDbHelper taskDbHelper = app.getTaskDbHelper();
-        List<Task> tasks = taskDbHelper.getTasks();
+        List<Task> tasks = taskDbHelper.getTasks(params);
+
+        /* TODO */
         Task task1 = new Task(1,1,"test1");
         task1.setImage(true);
         tasks.add(task1);
         tasks.add(new Task(2,2,"test2"));
         tasks.add(new Task(3,3,"test3 big big big so big test3 big big big so big1 1234 1234")); //60
-
         tasks.add(new Task(4,4,"test4"));
-        //tasks.add(new Task(5,5,"test1"));
-        /* TODO */
+
         return tasks;
     }
 }
