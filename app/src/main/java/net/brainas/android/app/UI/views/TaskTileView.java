d2eb171a6f8b18aa58db02bc893a03a649e16dec
Kit Ushakov <kitushakoff@gmail.com>
2015-11-27 18:39:04 +0300

    Alpha 0.02

--- app/src/main/java/net/brainas/android/app/UI/views/TaskTileView.java
+++ app/src/main/java/net/brainas/android/app/UI/views/TaskTileView.java
@@ -3,13 +3,12 @@ package net.brainas.android.app.UI.views;
 import android.content.Context;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import net.brainas.android.app.BrainasAppException;
 import net.brainas.android.app.R;
 import net.brainas.android.app.domain.models.Task;
 import net.brainas.android.app.infrustructure.InfrustructureHelper;
@@ -22,31 +21,23 @@ public class TaskTileView extends LinearLayout {
 
     public TaskTileView(final Context context, Task task) {
         super(context);
-        LayoutInflater.from(context).inflate(R.layout.task_tile_view, this, true);
+        LayoutInflater.from(context).inflate(R.layout.view_task_tile, this, true);
 
         this.task = task;
         final TaskTileView taskTile = this;
         taskTile.post(new Runnable() {
             @Override
             public void run() {
-                ImageView taskImageView = (ImageView)taskTile.findViewById(R.id.taskImage);
-                TextView taskMessageView = (TextView)taskTile.findViewById(R.id.taskMessage);
-
                 if (taskTile.task.haveImage()) {
-                    int imageSize = (int)((double)taskTile.getWidth()/1.5);
-                    int marginTop = (int)((float)imageSize/4);
-                    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(imageSize,imageSize);
-                    layoutParams.setMargins(0, marginTop, 0, 0);
-                    layoutParams.gravity= Gravity.CENTER;
-                    taskImageView.setLayoutParams(layoutParams);
-                    taskImageView.setImageBitmap(InfrustructureHelper.getTaskImage(taskTile.task));
-                    ((ViewGroup)taskMessageView.getParent()).removeView(taskMessageView);
+                    try {
+                        setTaskImage(taskTile);
+                    } catch (BrainasAppException e) {
+                        // e.getMessage()
+                        e.printStackTrace();
+                        setMessageText(taskTile);
+                    }
                 } else {
-                    ((LinearLayout) (taskMessageView.getParent())).setGravity(Gravity.CENTER);
-                    taskMessageView.setGravity(Gravity.CENTER);
-                    taskMessageView.setTextSize(taskTile.getTextSize());
-                    taskMessageView.setText(taskTile.task.getMessage());
-                    ((ViewGroup)taskImageView.getParent()).removeView(taskImageView);
+                    setMessageText(taskTile);
                 }
             }
         });
@@ -55,5 +46,34 @@ public class TaskTileView extends LinearLayout {
     private int getTextSize() {
         int textSize = (int)((double) this.getWidth()/20); // 35 - coun of symbol/4
         return textSize;
+    }
+
+    private void setTaskImage(TaskTileView taskTile) throws BrainasAppException {
+        ImageView taskImageView = (ImageView)taskTile.findViewById(R.id.taskImage);
+        TextView taskMessageView = (TextView)taskTile.findViewById(R.id.taskMessage);
+        int imageSize = (int)((double)taskTile.getWidth()/1.5);
+        int marginTop = (int)((float)imageSize/4);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(imageSize,imageSize);
+        layoutParams.setMargins(0, marginTop, 0, 0);
+        layoutParams.gravity= Gravity.CENTER;
+        taskImageView.setLayoutParams(layoutParams);
+        taskImageView.setImageBitmap(InfrustructureHelper.getTaskImage(taskTile.task));
+        ((ViewGroup)taskMessageView.getParent()).removeView(taskMessageView);
+    }
+
+    private void setMessageText(TaskTileView taskTile) {
+        ImageView taskImageView = (ImageView)taskTile.findViewById(R.id.taskImage);
+        TextView taskMessageView = (TextView)taskTile.findViewById(R.id.taskMessage);
+        ((LinearLayout) (taskMessageView.getParent())).setGravity(Gravity.CENTER);
+        taskMessageView.setGravity(Gravity.CENTER);
+        taskMessageView.setTextSize(taskTile.getTextSize());
+        taskMessageView.setText(taskTile.task.getMessage());
+        ((ViewGroup)taskImageView.getParent()).removeView(taskImageView);
+    }
+
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
     }
 }
