0fca22de0728386a5a1e33a05f295fdf959ea92a
Kit Ushakov <kitushakoff@gmail.com>
2015-12-04 16:27:57 +0300

    Alpha 0.02

---
+++ app/src/main/java/net/brainas/android/app/domain/helpers/NotificationManager.java
@@ -0,0 +1,16 @@
+package net.brainas.android.app.domain.helpers;
+
+import net.brainas.android.app.domain.models.Task;
+
+/**
+ * Created by innok on 11/30/2015.
+ */
+public class NotificationManager {
+    public NotificationManager() {}
+
+    public void notifyAboutTask(Task task) {
+        //TODO
+    }
+}
+
+
