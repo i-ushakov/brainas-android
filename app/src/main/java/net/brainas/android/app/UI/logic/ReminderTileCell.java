5eb4ce306f2b261ffa1fe19f9774e95fc64db4ad
Kit Ushakov <kitushakoff@gmail.com>
2015-12-05 13:27:31 +0300

    Alpha 0.02 - ReminderTileCell refactoring

--- app/src/main/java/net/brainas/android/app/UI/logic/ReminderTileCell.java
+++ app/src/main/java/net/brainas/android/app/UI/logic/ReminderTileCell.java
@@ -5,11 +5,11 @@ import android.graphics.Point;
 /**
  * Created by Kit Ushakov on 11/8/2015.
  */
-public class TileCell {
+public class ReminderTileCell {
     private Point position = new Point();
     private int size = 0;
 
-    TileCell(int positionX, int positionY,  int size) {
+    ReminderTileCell(int positionX, int positionY, int size) {
         this.position.set(positionX, positionY);
         this.size = size;
     }
