68ea412cd812563ffd7e2dfcac3eaa0f9e6123ea
Kit Ushakov <kitushakoff@gmail.com>
2015-12-12 13:12:23 +0300

    Alpha 0.03 - Maps Fragment (refactoring)

--- app/src/main/java/net/brainas/android/app/activities/TaskCardActivity.java
+++ app/src/main/java/net/brainas/android/app/activities/TaskCardActivity.java
@@ -59,21 +59,6 @@ public class TaskCardActivity extends AppCompatActivity {
         this.task = task;
 
         fillTheCardWithTaskInfo();
-        View conditon1EventsRow = findViewById(R.id.condition_1_events);
-        final View conditon1Details = TaskCardActivity.this.findViewById(R.id.condition_1_details);
-        conditon1Details.setVisibility(View.GONE);
-        conditon1EventsRow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (conditon1Details.getVisibility() == View.VISIBLE) {
-                    collapseConditionDetails(conditon1Details);
-                } else {
-                    expandConditionsDetails(conditon1Details);
-                }
-            }
-        });
-
-        MapFragment.newInstance(this, R.id.mapContainer, new LatLng(-33.867, 151.206));
     }
 
     @Override
@@ -81,60 +66,6 @@ public class TaskCardActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.task_card, menu);
         return true;
-    }
-
-    public static void expandConditionsDetails(final View v) {
-        v.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        final int targetHeight = v.getMeasuredHeight();
-
-        // Older versions of android (pre API 21) cancel animations for views with a height of 0.
-        v.getLayoutParams().height = 1;
-        v.setVisibility(View.VISIBLE);
-        Animation a = new Animation()
-        {
-            @Override
-            protected void applyTransformation(float interpolatedTime, Transformation t) {
-                v.getLayoutParams().height = interpolatedTime == 1
-                        ? ViewGroup.LayoutParams.WRAP_CONTENT
-                        : (int)(targetHeight * interpolatedTime);
-                v.requestLayout();
-            }
-
-            @Override
-            public boolean willChangeBounds() {
-                return true;
-            }
-        };
-
-        // 1dp/ms
-        a.setDuration((int)(targetHeight / v.getContext().getResources().getDisplayMetrics().density));
-        v.startAnimation(a);
-    }
-
-    public static void collapseConditionDetails(final View v) {
-        final int initialHeight = v.getMeasuredHeight();
-
-        Animation a = new Animation()
-        {
-            @Override
-            protected void applyTransformation(float interpolatedTime, Transformation t) {
-                if(interpolatedTime == 1){
-                    v.setVisibility(View.GONE);
-                }else{
-                    v.getLayoutParams().height = initialHeight - (int)(initialHeight * interpolatedTime);
-                    v.requestLayout();
-                }
-            }
-
-            @Override
-            public boolean willChangeBounds() {
-                return true;
-            }
-        };
-
-        // 1dp/ms
-        a.setDuration((int)(initialHeight / v.getContext().getResources().getDisplayMetrics().density));
-        v.startAnimation(a);
     }
 
     private void fillTheCardWithTaskInfo() {
@@ -154,9 +85,5 @@ public class TaskCardActivity extends AppCompatActivity {
             conditionsCont.addView(conditionBlock);
         }
     }
-
-    //private View makeEventView(Event event) {
-
-    //}
 }
 
