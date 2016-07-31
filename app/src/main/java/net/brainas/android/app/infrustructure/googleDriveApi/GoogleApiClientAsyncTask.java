8cbff2456b635b52e72cfd359e3843c788ce07ff
Kit Ushakov <kitushakoff@gmail.com>
2016-07-31 09:38:59 +0300

    Fix problem with driveId from google == null (v. 0.09)

---
+++ app/src/main/java/net/brainas/android/app/infrustructure/googleDriveApi/GoogleApiClientAsyncTask.java
@@ -0,0 +1,66 @@
+package net.brainas.android.app.infrustructure.googleDriveApi;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by innok on 6/30/2016.
+ */
+public abstract class GoogleApiClientAsyncTask<Params, Progress, Result>
+        extends AsyncTask<Params, Progress, Result> {
+
+    private GoogleApiClient mGoogleApiClient;
+
+    public GoogleApiClientAsyncTask(GoogleApiClient mClient) {
+        this.mGoogleApiClient = mClient;
+    }
+
+    @Override
+    protected final Result doInBackground(Params... params) {
+        Log.d("TAG", "in background");
+        final CountDownLatch latch = new CountDownLatch(1);
+        mGoogleApiClient.registerConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+            @Override
+            public void onConnectionSuspended(int cause) {
+            }
+
+            @Override
+            public void onConnected(Bundle arg0) {
+                latch.countDown();
+            }
+        });
+        mGoogleApiClient.registerConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+            @Override
+            public void onConnectionFailed(ConnectionResult arg0) {
+                latch.countDown();
+            }
+        });
+        mGoogleApiClient.connect();
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            return null;
+        }
+        if (!mGoogleApiClient.isConnected()) {
+            return null;
+        }
+        try {
+            return doInBackgroundConnected(params);
+        } finally {
+            //mGoogleApiClient.disconnect();
+        }
+    }
+
+    /**
+     * Override this method to perform a computation on a background thread, while the client is
+     * connected.
+     */
+    protected abstract Result doInBackgroundConnected(Params... params);
+
+}
