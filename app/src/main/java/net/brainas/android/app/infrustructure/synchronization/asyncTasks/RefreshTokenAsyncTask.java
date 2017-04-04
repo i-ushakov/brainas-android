1a73df59e0b58385117b94149ab7cea20c92a055
Kit Ushakov <kit.ushakoff@gmail.com>
2017-04-04 22:16:16 +0300

    Added buils method

--- app/src/main/java/net/brainas/android/app/infrustructure/synchronization/asyncTasks/RefreshTokenAsyncTask.java
+++ app/src/main/java/net/brainas/android/app/infrustructure/synchronization/asyncTasks/RefreshTokenAsyncTask.java
@@ -1,6 +1,5 @@
 package net.brainas.android.app.infrustructure.synchronization.asyncTasks;
 
-import android.nfc.Tag;
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -11,13 +10,11 @@ import net.brainas.android.app.infrustructure.SyncHelper;
 import net.brainas.android.app.infrustructure.UserAccount;
 import net.brainas.android.app.services.SynchronizationService;
 
-import org.json.JSONException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
 
@@ -28,18 +25,33 @@ import javax.xml.parsers.ParserConfigurationException;
 /**
  * Created by Kit Ushakov on 5/9/2016.
  */
-public class RefreshTokenAsyncTask extends AsyncTask<UserAccount, Void, String> {
+public class RefreshTokenAsyncTask extends AsyncTask<Void, Void, String> {
     static String TAG = "RefreshTokenAsyncTask";
     private ResponseListener mListener = null;
     private Exception mError = null;
     private UserAccount userAccount = null;
 
+    public static RefreshTokenAsyncTask build(UserAccount userAccount) {
+        final RefreshTokenAsyncTask refreshTokenAsyncTask = new RefreshTokenAsyncTask(userAccount);
+        refreshTokenAsyncTask.setListener(new RefreshTokenAsyncTask.ResponseListener() {
+
+            @Override
+            public void onComplete(String response, Exception e) {
+                refreshTokenAsyncTask.handleResponse(response);
+            }
+        });
+
+        return refreshTokenAsyncTask;
+    }
+
+    protected RefreshTokenAsyncTask(UserAccount userAccount) {
+       this.userAccount = userAccount;
+    }
+
     @Override
-    protected String doInBackground(UserAccount... params) {
+    protected String doInBackground(Void... params) {
         String response = null;
-        if (params[0] instanceof UserAccount) {
-            this.userAccount = params[0];
-        }
+
         // send changes to server for processing
         if (NetworkHelper.isNetworkActive()) {
             response = SyncHelper.refreshAccessTokenRequest();
@@ -75,7 +87,10 @@ public class RefreshTokenAsyncTask extends AsyncTask<UserAccount, Void, String> 
     }
 
     public void handleResponse(String response) {
-        if (response != null) {
+        if (response == null) {
+            CLog.e(TAG, "Response is NULL", null);
+            return;
+        } else {
             CLog.i(TAG, response);
         }
         try {
